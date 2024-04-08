c524b296a8d76db620158ed0b176150ef7e97ed7
chlinyang97 <104578692+chlinyang97@users.noreply.github.com>
2024-04-08 00:07:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def index():
     return send_file('diagram.png', mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
